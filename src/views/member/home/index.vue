96c90b05a859fd1e67750d9aad83db4ee8f097a8
xiaozhou <zxh04551@163.com>
2022-03-22 19:04:44 +0800

    项目完成

--- src/views/member/home/index.vue
+++ src/views/member/home/index.vue
@@ -3,9 +3,13 @@
     <!-- 概况 -->
     <HomeOverview />
     <!-- 收藏 -->
-    <HomePanel title="我的收藏" />
+    <HomePanel title="我的收藏">
+      <GoodsItem v-for="i in 4" :key="i" :goods="goods" />
+    </HomePanel>
     <!-- 足迹 -->
-    <HomePanel title="我的足迹" />
+    <HomePanel title="我的足迹">
+      <GoodsItem v-for="i in 4" :key="i" :goods="goods" />
+    </HomePanel>
     <!-- 猜你喜欢 -->
     <GoodsRecommend />
   </div>
@@ -15,8 +19,15 @@
 import HomeOverview from "./HomeOverview.vue"
 import HomePanel from "./HomePanel.vue"
 import GoodsRecommend from "../../goods/GoodsRecommend.vue"
-import { ref } from "vue"
+import GoodsItem from "../../category/GoodsItem.vue"
 
+const goods = {
+  id: "1",
+  name: "自煮火锅不排队 麦饭石不粘鸳鸯火锅",
+  picture: "https://yanxuan-item.nosdn.127.net/fcdcb840a0f5dd754bb8fd2157579012.jpg",
+  desc: "清汤鲜香 红汤劲爽",
+  price: "159.00"
+}
 </script>
 
 <style scoped lang="less">
